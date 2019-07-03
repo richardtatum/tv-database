6d063040750b378e0f0cb40bdde436380cdc9611
richardtatum <DP@DP.com>
2019-07-03 14:17:11 +0100

    Changed formatting of data to a list for openpyxl.append()

--- main.py
+++ main.py
@@ -4,18 +4,24 @@ import requests
 def parse_content(url):
     r = requests.get(url)
     soup = bs(r.text, 'html.parser')
+    data = []
 
+    # Section that holds the date and country
     link_data = soup.find('div', class_='profile_tweet_date_country').get_text().split()
 
-    data = {}
-    data['date'] = link_data[0]
-    data['country'] = ' '.join(link_data[2:])
-    data['title'] = soup.find('div', class_='profile_tweet_title').get_text()
-    data['content'] = soup.find('div', class_='profile_tweet_content').get_text()
+    # Format the date to requested style
+    time = datetime.strptime(link_data[0], '%d/%m/%Y').strftime('%d.%m.%y')
 
-    print(data)
+    # Appending data to a list (required for openpyxl.append())
+    data.append(time)
+    data.append(' '.join(link_data[2:]))
+    data.append(soup.find('div', class_='profile_tweet_content').get_text())
+
+    # Pass data to worksheet
+    insert_data(data)
 
 
-url = 'http://tvbizz.net/newsitemsocial?newsId=128888'
-# url = 'http://tvbizz.net/newsitemsocial?newsId=128909'
-parse_content(url)
+urls = ['http://tvbizz.net/newsitemsocial?newsId=128888','http://tvbizz.net/newsitemsocial?newsId=128937',
+        'http://tvbizz.net/newsitemsocial?newsId=128936', 'http://tvbizz.net/newsitemsocial?newsId=128931']
+for url in urls:
+    parse_content(url)
